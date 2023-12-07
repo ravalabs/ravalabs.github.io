36662b1cb5abe26e46e2cf0a6d2a29440c8e9b12
ravalabs <ravarixlabs@gmail.com>
2023-12-07 14:44:05 +0100

    test

--- js/taruguitos_traits.js
+++ js/taruguitos_traits.js
@@ -1,4 +1,4 @@
-function createTbodyAttributes(items) {
+function createTbodyNames(items) {
     var table = document.getElementById("table_traits");
 
     // Eliminar el Tbody con los objetos que contenga
@@ -10,17 +10,11 @@ function createTbodyAttributes(items) {
     // Agrega las filas dependiendo el número de items
     for (let i = 0; i < items; i++) {
         var tr = document.createElement('tr');
-        var td_id = document.createElement('td');
-        var td_trait_type = document.createElement('td');
-        var td_value = document.createElement('td');
+        var td_name = document.createElement('td');
 
-        td_id.id = "attribute_id" + i;
-        td_trait_type.id = "attribute_trait_type" + i;
-        td_value.id = "attribute_value" + i
+        td_name.id = "name_value" + i
 
-        tr.appendChild(td_id);
-        tr.appendChild(td_trait_type);
-        tr.appendChild(td_value);
+        tr.appendChild(td_name);
 
         tbody_traits.appendChild(tr);
     }
@@ -34,32 +28,30 @@ async function getData(url) {
     return response.json();
 }
 
-window.setInterval(async function getAttributes() {
+window.setInterval(async function getNames() {
     // Establece la ruta del archivo JSON
     const url = "../datos/datos.json";
     const data = await getData(url);
 
-    // Attributes toma los datos del objeto attributes
-    let attributes = data["meta"];
+    // Names toma los datos del objeto names
+    let names = data["meta"];
     
     // Se obtiene el numero de elementos
-    let items = Object.keys(attributes).length
+    let items = Object.keys(names).length
 
     // Se crea un Tbody con el numero de filas
-    createTbodyAttributes(items);
+    createTbodyNames(items);
 
     // establecemos un contador para el siguiente bucle
     contador = 0
 
     // recorremos las propiedades del objeto 
-    for (let key in attributes) {
+    for (let key in names) {
       var td_id = document.getElementById('attribute_id' + contador);
       var td_trait_type = document.getElementById('attribute_trait_type' + contador);
       var td_value = document.getElementById('attribute_value' + contador);
 
-      td_id.innerHTML = attributes[key][0].id;
-      td_trait_type.innerHTML = attributes[key][0].trait_type;
-      td_value.innerHTML = attributes[key][0].value;
+      td_name.innerHTML = names[key][0].value;
       contador++
     }
 }, 1000);
